16d9322bf4c999592eeb1e5b611ad0f2f2abf60b
Rafael Riber <rafael@riber.ch>
2025-04-19 12:44:42 +0200

    Modify things to be able to use it as a package

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="simconnect_mobiflight",
     version="0.3.0",
-    packages=find_packages(),
-    install_requires=["SimConnect"],
+    packages=find_packages(include=["simconnect_mobiflight*"]),
+    install_requires=["SimConnect>=0.1.0"],
     author="Koseng",
-    description="Unofficial SimConnect extension for MobiFlight InputEvents support",
+    description="SimConnect Python extension for MobiFlight InputEvents",
     url="https://github.com/Koseng/MSFSPythonSimConnectMobiFlightExtension",
+    classifiers=["Programming Language :: Python :: 3"],
+    python_requires=">=3.10",
 )
---
+++ simconnect_mobiflight/src/__init__.py
@@ -0,0 +1,4 @@
+from .simconnect_mobiflight import SimConnectMobiFlight
+from .mobiflight_variable_requests import MobiflightVariableRequests
+
+__all__ = ["SimConnectMobiFlight", "MobiflightVariableRequests"]
